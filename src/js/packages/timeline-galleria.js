f9ab42bb3933a82bd9bf1ac30df54eb9012dc26c
Rodrigo <rodrigovieira18@gmail.com>
2015-08-13 14:12:18 -0300

    Fixed Galleria bug when we dont have buzz  to start

--- src/js/packages/timeline-galleria.js
+++ src/js/packages/timeline-galleria.js
@@ -1,5 +1,3 @@
 $(document).ready(function() {
   Galleria.loadTheme('//cdnjs.cloudflare.com/ajax/libs/galleria/1.4.2/themes/classic/galleria.classic.min.js');
-  Galleria.run('.gallery section.photos');
-  $('.gallery section.photos').addClass('gallery-on');
 });
