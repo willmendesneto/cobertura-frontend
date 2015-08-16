c1509bf6423551b15a6e69268056b23375a1da4d
Will Mendes <willmendesneto@gmail.com>
2015-08-16 01:01:50 -0300

    Add social share in timeline text block

--- src/js/components/timeline/renderers/TimeLineBlockText.js
+++ src/js/components/timeline/renderers/TimeLineBlockText.js
@@ -1,6 +1,7 @@
 (function(window) {
   'use strict';
   var TimeLineBlockText = {};
+  var CONFIG = window.CONFIG;
 
   extend(TimeLineBlockText, window.ITimeLineBlock);
 
@@ -18,6 +19,18 @@
       '<article class="timeline-content text">' +
         '<h2 class="estado">' + data.local + '</h2>' +
     '    <p>' + data.content + '</p>' +
+        '<div class="logos">'+
+          '<div class="social-small">'+
+            '<a target="_blank" href="//www.twitter.com/share?url=' + CONFIG.URL_COBERTURA + '&via=Brasil_de_Fato&related=Brasil_de_Fato&text=' + document.title + '" >' +
+              '<i class="fa fa-twitter"></i>'+
+            '</a>' +
+          '</div>'+
+          '<div class="social-small">' +
+            '<a href="//www.facebook.com/dialog/feed?app_id=' + CONFIG.FACEBOOK_ID + '&display=popup&href=' + CONFIG.URL_COBERTURA + '&redirect_uri=' + CONFIG.URL_COBERTURA + '&picture=' + CONFIG.URL_SOCIAL_SHARE_IMAGE + '&name=' + document.title + '&description=' + data.content + '&link=' + CONFIG.URL_COBERTURA + '" target="_blank">' +
+              '<i class="fa fa-facebook"></i>' +
+            '</a>' +
+          '</div>' +
+        '</div>' +
     '  </article>' +
     '</div>';
   };
