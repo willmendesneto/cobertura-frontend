809da02e963346cc069b04a421693caad42caa23
Will Mendes <willmendesneto@gmail.com>
2015-08-20 00:37:24 -0300

    Add initial config tests

--- test/spec/components/timeline/constants/Config.js
+++ test/spec/components/timeline/constants/Config.js
@@ -7,8 +7,10 @@ describe('Config.js', function () {
     CONFIG = window.CONFIG;
   });
 
-  it('should return 2', function () {
-    expect(1 + 1).toBe(2);
+  it('Initial configurations', function () {
+    expect(CONFIG.FACEBOOK_ID).toBe(1500652936893411);
+    expect(CONFIG.URL_COBERTURA).toBe('http://cobertura.brasildefato.com.br');
+    expect(CONFIG.URL_SOCIAL_SHARE_IMAGE).toBe('http://cobertura.brasildefato.com.br/assets/img/logo.jpg');
   });
 
 });
