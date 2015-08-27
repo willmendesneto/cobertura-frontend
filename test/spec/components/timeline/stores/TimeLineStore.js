2f0bb0da4ba594f80ea74c81e6afd30dbcde64ee
Will Mendes <willmendesneto@gmail.com>
2015-08-27 13:26:08 -0300

    Add tests for "#remove" in TimelineStore

--- test/spec/components/timeline/stores/TimeLineStore.js
+++ test/spec/components/timeline/stores/TimeLineStore.js
@@ -2,10 +2,13 @@ describe('TimelineStore.js', function () {
   'use strict';
 
   var store;
-  var data = [{ title:1 }, {title:2}, { title:3 }, {title:4}, { title:5 }, {title:6}, { title:7 }, {title:8}, { title:9 }, {title:10}, {title:11}, { title:10 }, {title:11}, {title:12}, { title:13 }, {title:14}];
+  var data;
 
   beforeEach(function () {
     store = window.TimeLineStore;
+    data = [{ title:1 }, {title:2}, { title:3 }, {title:4}, { title:5 }, {title:6}, { title:7 }, {title:8}, { title:9 }, {title:10}, {title:11}, { title:10 },
+      {title:11}, {title:12}, { title:13 }, {title:14}];
+
   });
 
   it('should save data', function () {
@@ -15,6 +18,26 @@ describe('TimelineStore.js', function () {
   it('should calc number of pages to pagination', function () {
     store.setData(data);
     expect(store.numberOfPages()).toBe(2);
+  });
+
+  it('#remove', function () {
+    store.setData(data);
+    var currentLength = data.length - 1;
+
+    store.remove(data[0]);
+
+    var dataStore = [];
+    var localStore;
+    localStore = store.getLocalOldestInformations();
+    while (localStore.length > 0) {
+      localStore.forEach(function(element, index) {
+        dataStore.push(element);
+        store.remove(element);
+      });
+      localStore = store.getLocalOldestInformations();
+    }
+
+    expect(currentLength).toBe(dataStore.length);
   });
 
   describe('getLocalOldestInformations',function(){
