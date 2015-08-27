f6a042d3faf0193bf1eef8b4ce0542136f142ce8
Patrick Maia <patrickjourdanmaia@gmail.com>
2015-08-27 17:42:33 -0300

    gets proper configuration according with the environment

--- src/js/components/timeline/constants/Config.js
+++ src/js/components/timeline/constants/Config.js
@@ -1,12 +1,6 @@
 (function(window) {
   'use strict';
 
-  function getSocketIOUrl(){
-    return window.location.origin.indexOf('localhost') === -1 ?
-            '//burburinho.herokuapp.com' :
-            '//localhost:5000';
-  }
-
   function checkUrlBufferInfoWhenAppIsOffline(){
     var offline = window.offline || false;
     return !!offline ?
@@ -14,13 +8,42 @@
             getSocketIOUrl()+'/api/buzzes';
   }
 
-  var CONFIG = {
+  var CONFIG_LOCAL = {
+    FACEBOOK_ID: 1500652936893411,
+    URL_COBERTURA: 'http://localhost:4000',
+    URL_SOCIAL_SHARE_IMAGE: 'http://localhost:4000/assets/img/logo.jpg',
+    URL_BUFFER_INFO: checkUrlBufferInfoWhenAppIsOffline(),
+    URL_SOCKET_IO: '//localhost:5000'
+  };
+
+  var CONFIG_STAGING = {
+    FACEBOOK_ID: 1500652936893411,
+    URL_COBERTURA: 'https://test-cobertura.herokuapp.com',
+    URL_SOCIAL_SHARE_IMAGE: 'https://test-cobertura.herokuapp.com/assets/img/logo.jpg',
+    URL_BUFFER_INFO: checkUrlBufferInfoWhenAppIsOffline(),
+    URL_SOCKET_IO: '//test-burburinho.herokuapp.com'
+  };
+
+  var CONFIG_PRODUCTION = {
     FACEBOOK_ID: 1500652936893411,
     URL_COBERTURA: 'http://cobertura.brasildefato.com.br',
     URL_SOCIAL_SHARE_IMAGE: 'http://cobertura.brasildefato.com.br/assets/img/logo.jpg',
     URL_BUFFER_INFO: checkUrlBufferInfoWhenAppIsOffline(),
-    URL_SOCKET_IO: getSocketIOUrl()
+    URL_SOCKET_IO: '//burburinho.herokuapp.com'
   };
+
+  function getConfiguration(){
+    var origin = window.location.origin; 
+    if(origin.indexOf('localhost')) !== -1 {
+        return CONFIG_LOCAL;
+    } else if(origin.indexOf('test') !== -1) {
+        return CONFIG_STAGING;
+    } else {
+        return CONFIG_PRODUCTION;
+    }
+  };
+
+  var CONFIG = getConfiguration(); 
 
   if (typeof define !== 'undefined' && define.amd) {
     // AMD. Register as an anonymous module.
