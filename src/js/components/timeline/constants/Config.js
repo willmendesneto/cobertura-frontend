a2ec9b72208a2552e2a910ddf4e27c1758dec02e
Patrick Maia <patrickjourdanmaia@gmail.com>
2015-08-28 15:48:15 -0300

    bug fix: gets the right URL for news feed

--- src/js/components/timeline/constants/Config.js
+++ src/js/components/timeline/constants/Config.js
@@ -1,18 +1,10 @@
 (function(window) {
   'use strict';
 
-  function checkUrlBufferInfoWhenAppIsOffline(){
-    var offline = window.offline || false;
-    return !!offline ?
-            '/assets/json/'+offline.data :
-            getSocketIOUrl()+'/api/buzzes';
-  }
-
   var CONFIG_LOCAL = {
     FACEBOOK_ID: 1500652936893411,
     URL_COBERTURA: 'http://localhost:4000',
     URL_SOCIAL_SHARE_IMAGE: 'http://localhost:4000/assets/img/logo.jpg',
-    URL_BUFFER_INFO: checkUrlBufferInfoWhenAppIsOffline(),
     URL_SOCKET_IO: '//localhost:5000'
   };
 
@@ -20,7 +12,6 @@
     FACEBOOK_ID: 1500652936893411,
     URL_COBERTURA: 'https://test-cobertura.herokuapp.com',
     URL_SOCIAL_SHARE_IMAGE: 'https://test-cobertura.herokuapp.com/assets/img/logo.jpg',
-    URL_BUFFER_INFO: checkUrlBufferInfoWhenAppIsOffline(),
     URL_SOCKET_IO: '//test-burburinho.herokuapp.com'
   };
 
@@ -28,7 +19,6 @@
     FACEBOOK_ID: 1500652936893411,
     URL_COBERTURA: 'http://cobertura.brasildefato.com.br',
     URL_SOCIAL_SHARE_IMAGE: 'http://cobertura.brasildefato.com.br/assets/img/logo.jpg',
-    URL_BUFFER_INFO: checkUrlBufferInfoWhenAppIsOffline(),
     URL_SOCKET_IO: '//burburinho.herokuapp.com'
   };
 
@@ -44,6 +34,13 @@
   };
 
   var CONFIG = getConfiguration(); 
+
+  CONFIG.getUrlBufferInfo = function() {
+    var offline = window.offline || false;
+    return !!offline ?
+            '/assets/json/'+offline.data :
+            CONFIG.URL_SOCKET_IO + '/api/buzzes';
+  }
 
   if (typeof define !== 'undefined' && define.amd) {
     // AMD. Register as an anonymous module.
